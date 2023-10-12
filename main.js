54e9937b30d0d8d1d3b269cea24ec33bf16dddd3
Vu Huy <vuhuycto@gmail.com>
2023-10-13 00:13:14 +0700

    update tour info

--- main.js
+++ main.js
@@ -1,30 +1,25 @@
-const introLayoutElm = document.querySelector(".intro-layout");
-const songs = document.getElementById("songs");
-
-function setSongsMarginLeft() {
+document.addEventListener("DOMContentLoaded", function () {
+  const introLayoutElm = document.querySelector("nav");
   const introLayoutElmStyle =
     introLayoutElm.currentStyle || window.getComputedStyle(introLayoutElm);
-  const currentMargin = `${
-    parseInt(introLayoutElmStyle.marginLeft.replace("px", "")) +
-    parseInt(
-      introLayoutElmStyle.getPropertyValue("padding-left").replace("px", "")
-    )
-  }px`;
-
-  songs.style.paddingLeft = currentMargin;
-  songs.style.paddingRight = currentMargin;
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  setSongsMarginLeft();
+  $(".owl-carousel").owlCarousel({
+    stagePadding:
+      parseInt(introLayoutElmStyle.marginLeft.replace("px", "")) +
+      parseInt(
+        introLayoutElmStyle.getPropertyValue("padding-left").replace("px", "")
+      ),
+    loop: false,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      640: {
+        items: 2,
+      },
+      1024: {
+        items: 3,
+      },
+    },
+  });
 });
-
-songs.onscroll = function (evt) {
-  document.querySelector(".progress-bar").style.width = `${
-    (100 * songs.scrollLeft) / (songs.scrollWidth - songs.clientWidth)
-  }%`;
-};
-
-window.onresize = function (evt) {
-  setSongsMarginLeft();
-};
