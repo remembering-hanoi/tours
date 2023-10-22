c78edd8fc07c511c833c0844846cfbd7dd1972bf
Vu Huy <vuhuycto@gmail.com>
2023-10-22 12:47:41 +0700

    update carousel

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
+  $(".customer-smiles").owlCarousel({
+    center: true,
+    items: 2,
+    loop: true,
+    margin: 12,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      640: {
+        items: 2,
+      },
+    },
+  });
+
   const introLayoutElm = document.querySelector("nav");
   const introLayoutElmStyle =
     introLayoutElm.currentStyle || window.getComputedStyle(introLayoutElm);
-  $(".owl-carousel").owlCarousel({
+  $(".customer-experiences").owlCarousel({
     stagePadding:
       parseInt(introLayoutElmStyle.marginLeft.replace("px", "")) +
       parseInt(
